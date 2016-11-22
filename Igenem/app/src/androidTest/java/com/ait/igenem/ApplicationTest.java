5320b4e29ae8639f9cd344f2efce3ec68f1ec204
Emily Chen <echen383@gmail.com>
2016-11-22 23:11:22 +0100

    new project with LoginActivity

--- Igenem/app/src/androidTest/java/com/ait/igenem/ApplicationTest.java
+++ Igenem/app/src/androidTest/java/com/ait/igenem/ApplicationTest.java
@@ -1,4 +1,4 @@
-package com.igenem;
+package com.ait.igenem;
 
 import android.app.Application;
 import android.test.ApplicationTestCase;
--- Igenem/app/src/test/java/com/ait/igenem/ExampleUnitTest.java
+++ Igenem/app/src/test/java/com/ait/igenem/ExampleUnitTest.java
@@ -1,4 +1,4 @@
-package com.igenem;
+package com.ait.igenem;
 
 import org.junit.Test;
 
