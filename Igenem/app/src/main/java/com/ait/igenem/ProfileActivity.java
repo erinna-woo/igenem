748bdc1e4983de3205bec189a3146ac9a5244e08
Emily Chen <echen383@gmail.com>
2016-11-22 23:55:10 +0100

    added activities
    HomeActivity, ProfileActivity, CreateDecisionActivity, DecisionActivity

---
+++ Igenem/app/src/main/java/com/ait/igenem/ProfileActivity.java
@@ -0,0 +1,15 @@
+package com.ait.igenem;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ProfileActivity extends AppCompatActivity {
+
+    // user profile
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+    }
+}
