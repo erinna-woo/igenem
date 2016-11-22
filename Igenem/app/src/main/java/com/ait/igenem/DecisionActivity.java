47c800b09ddfb7060a83b79a7e19b04d9db36fe7
Erinna Woo <erinna.woo@pomona.edu>
2016-11-23 00:09:37 +0100

    Created Decision and User classes

--- Igenem/app/src/main/java/com/ait/igenem/DecisionActivity.java
+++ Igenem/app/src/main/java/com/ait/igenem/DecisionActivity.java
@@ -10,4 +10,5 @@ public class DecisionActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_decision);
     }
+    //TODO
 }
---
+++ Igenem/app/src/main/java/com/ait/igenem/model/Decision.java
@@ -0,0 +1,8 @@
+package com.ait.igenem.model;
+
+/**
+ * Created by Erinna on 11/23/16.
+ */
+
+public class Decision {
+}
