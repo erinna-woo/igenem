530749e44d00d216334a586b990eedf86fc23c18
Emily Chen <echen383@gmail.com>
2016-11-24 14:03:41 +0100

    filled in decision class
    also added factor class

--- Igenem/app/src/main/java/com/ait/igenem/model/Decision.java
+++ Igenem/app/src/main/java/com/ait/igenem/model/Decision.java
@@ -1,8 +1,42 @@
 package com.ait.igenem.model;
 
+import java.util.List;
+
 /**
  * Created by Erinna on 11/23/16.
  */
 
 public class Decision {
+
+    private String name;
+    private String color;
+    private List<Factor> factorList;
+
+    public Decision(String name, String color, List<Factor> factorList) {
+        this.name = name;
+        this.color = color;
+        this.factorList = factorList;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {  this.name = name;   }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public List<Factor> getFactorList() {
+        return factorList;
+    }
+
+    public void setFactorList(List<Factor> factorList) {
+        this.factorList = factorList;
+    }
 }
