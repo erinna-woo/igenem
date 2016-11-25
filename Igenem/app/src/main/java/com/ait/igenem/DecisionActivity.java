af71f590b6e1898885f555ac1db62061a9e327fb
Erinna Woo <erinna.woo@pomona.edu>
2016-11-25 20:41:50 +0100

    Creating basic decision activity functions

--- Igenem/app/src/main/java/com/ait/igenem/DecisionActivity.java
+++ Igenem/app/src/main/java/com/ait/igenem/DecisionActivity.java
@@ -2,13 +2,34 @@ package com.ait.igenem;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class DecisionActivity extends AppCompatActivity {
+
+    @BindView(R.id.btnNewBlob)
+    Button btnNewBlob;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_decision);
+        ButterKnife.bind(this);
+
+        //TODO: from button --> double click blob
+        setupNewBlobButton();
+    }
+
+    private void setupNewBlobButton() {
+        btnNewBlob.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
     //TODO
 }
--- Igenem/app/src/main/java/com/ait/igenem/LoginActivity.java
+++ Igenem/app/src/main/java/com/ait/igenem/LoginActivity.java
@@ -1,13 +1,32 @@
 package com.ait.igenem;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class LoginActivity extends AppCompatActivity {
+
+    //temporary to open the decision activity. delete later
+    @BindView(R.id.btnOpenDecision)
+    Button btnOpenDecision;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        ButterKnife.bind(this);
+        btnOpenDecision.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent openDecision = new Intent();
+                openDecision.setClass(LoginActivity.this, DecisionActivity.class);
+                startActivity(openDecision);
+            }
+        });
     }
 }
