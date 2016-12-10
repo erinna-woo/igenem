8c1e4eb43eb0f680cb288a2f886df81ce506a463
Emily Chen <echen383@gmail.com>
2016-12-10 11:58:14 +0100

    updating background colors

--- Igenem/app/src/main/java/com/ait/igenem/adapter/DynamicBlobRecyclerAdapter.java
+++ Igenem/app/src/main/java/com/ait/igenem/adapter/DynamicBlobRecyclerAdapter.java
@@ -1,6 +1,5 @@
 package com.ait.igenem.adapter;
 
-import android.graphics.Color;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -10,8 +9,8 @@ import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.ait.igenem.PassDataDynamicBlobInterface;
-import com.ait.igenem.model.Blob;
 import com.ait.igenem.R;
+import com.ait.igenem.model.Blob;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -52,11 +51,9 @@ public class DynamicBlobRecyclerAdapter extends RecyclerView.Adapter<DynamicBlob
         if (dyamicBlobList.get(position).isPro()) {
             holder.tvDBlobScore.setText("Score: " + dyamicBlobList.get(holder.getAdapterPosition()).getRadius());
             holder.tvDProCon.setText("PRO");
-            holder.layoutDBlobRow.setBackgroundColor(Color.rgb(173, 244, 203));
         } else {
             holder.tvDBlobScore.setText("Score: " + dyamicBlobList.get(holder.getAdapterPosition()).getRadius());
             holder.tvDProCon.setText("CON");
-            holder.layoutDBlobRow.setBackgroundColor(Color.rgb(244, 173, 173));
         }
         setupEditBlobListener(holder, holder.key);
     }
