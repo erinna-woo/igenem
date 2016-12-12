44278c92e9942df390f45537db46975ec42277ea
Erinna Woo <erinna.woo@pomona.edu>
2016-12-12 18:23:45 +0100

    NOW NOW we're done

--- Igenem/app/src/main/java/com/ait/igenem/DecisionActivity.java
+++ Igenem/app/src/main/java/com/ait/igenem/DecisionActivity.java
@@ -5,7 +5,6 @@ import android.graphics.Color;
 import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
